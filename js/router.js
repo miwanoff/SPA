31b18ab2d3f854db31046d2b67ea2550a1eaf0bc
miwanoff <marina.iva.noffa@gmail.com>
2026-04-05 13:37:02 +0300

    Add full code

--- js/router.js
+++ js/router.js
@@ -1,7 +1,9 @@
 const route = (event) => {
   event.preventDefault();
   window.history.pushState({}, "", event.target.href);
+  handleLocation();
 };
+
 
 const routes = {
   404: "/pages/404.html",
@@ -10,9 +12,23 @@ const routes = {
   "/lorem": "/pages/lorem.html",
 };
 
+
 const handleLocation = async () => {
   const path = window.location.pathname;
   const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main-page").innerHTML = html;
 };
 
+
+window.onpopstate = handleLocation;
 window.route = route;
+
+
+handleLocation();
+
+
+document.querySelectorAll("#main-nav ul li a").forEach((a) => {
+  a.addEventListener("click", route);
+});
+
