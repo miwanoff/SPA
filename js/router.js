12164dc16364edb2226063c5c3b38d62c403b1c0
miwanoff <marina.iva.noffa@gmail.com>
2026-04-05 13:30:14 +0300

    Add content

--- js/router.js
+++ js/router.js
@@ -3,4 +3,16 @@ const route = (event) => {
   window.history.pushState({}, "", event.target.href);
 };
 
+const routes = {
+  404: "/pages/404.html",
+  "/": "/pages/index.html",
+  "/about": "/pages/about.html",
+  "/lorem": "/pages/lorem.html",
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+};
+
 window.route = route;
